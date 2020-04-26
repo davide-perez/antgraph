465fcb2f3ab75e3673f9c9bdcca6f382b889cf23
davide-perez <perezposta@gmail.com>
2020-04-26 12:51:48 +0100

    Infobox table now adds rows dynamically

--- public/models/environment/gnode.js
+++ public/models/environment/gnode.js
@@ -1,28 +1,11 @@
 class GNode {
 
-    constructor(label){
+    constructor(label) {
 
         this.label = label;
         this.id = undefined;
         this.isGoal = false;
         this.isStart = false;
     }
-
-// USE OBJECT DEFINE PROPERTY TO SET RULES FOR THIS ******** ID PLEASE
-// So that property cannot be assigned w/o enforcing some rules, because rename()
-// does not protect against misuses (node.id = somewrongval is still possible)
-
-/**
- * Generates a unique id code (wrt the application's domain) based on a timestamp and a random number.
- * @returns a string representing an id
- */
-
-    generateId(){
-        if (!Date.now) {
-            Date.now = function() { return new Date().getTime(); }
-        }
-        return "" + Math.round(Math.random() * Date.now());
-    }
-
 
 }
--- public/views/infobox.js
+++ public/views/infobox.js
@@ -1,15 +1,29 @@
 function updateGraphInfo(env) {
-  if (!env)
-    return;
-  $("#graph-info").empty();
+  $("#graph-info").html("");
+  if (!env) return;
   let noOfNodes = env.nodes.length || 0;
   let noOfEdges = env.links.length || 0;
-  $("#graph-info").after("<tr><td><b>" + noOfNodes + "</b></td><td>type here</td></tr>");
-  $("#graph-info").after("<tr><td><b>" + noOfEdges + "</b></td><td>type here</td></tr>");
+  $("#graph-info").append(
+    "<tr><td><b>No. of nodes</b></td><td>" + noOfNodes + "</td></tr>"
+  );
+  $("#graph-info").append(
+    "<tr><td><b>No. of edges</b></td><td>" + noOfEdges + "</td></tr>"
+  );
 }
 
 function showNodeInfo(node) {
-  if (!node)
-    return;
-  let infobox = $("#infobox");
+  $("#item-info").html("");
+  if (!node) return;
+  $("#item-info").append(
+    "<tr><td><b>Node name</b></td><td>" + noOfNodes + "</td></tr>"
+  );
+  $("#item-info").append(
+    "<tr><td><b>Node id</b></td><td>" + noOfEdges + "</td></tr>"
+  );
+  $("#item-info").append(
+    "<tr><td><b>Node class</b></td><td>" + noOfNodes + "</td></tr>"
+  );
+  $("#item-info").append(
+    "<tr><td><b>Node fbf</b></td><td>" + noOfEdges + "</td></tr>"
+  );
 }
